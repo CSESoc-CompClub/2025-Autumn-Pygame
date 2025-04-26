480a1d09bbe7083c29ff45e0b02e2dbeafee2d90
Eve Miles <eve.miles09@gmail.com>
2025-04-26 22:52:03 +1000

    Implement task 3 and optimise

--- game.py
+++ game.py
@@ -8,8 +8,8 @@ screen_size_y = 600
 screen = pygame.display.set_mode((screen_size_x, screen_size_y))
 
 # Task 1.3
-background = pygame.image.load("images/background/wooden_floor.jpeg")
-background = pygame.transform.scale(background, (screen_size_x, screen_size_y))
+background_image = "images/background/wooden_floor.jpeg"
+background = pygame.transform.scale(pygame.image.load(background_image), (screen_size_x, screen_size_y))
 screen.blit(background, (0, 0))
 
 # Task 1.4
@@ -19,39 +19,78 @@ pygame.display.set_icon(pygame.image.load("images/game_icon.png"))
 # Task 2.1 - create player class
 class Player:
     def __init__(self):
-        self.sprite = "images/player/poco_down.png"
         self.position_x = 0
         self.position_y = 0
-        self.player_size = (100, 100)
+        self.size = (100, 100)
+        self.image = "images/player/poco_down.png"
+        self.sprite = pygame.transform.scale(pygame.image.load(self.image), self.size)
         
     # Task 2.3 - create draw method
     def draw(self):
-        player = pygame.image.load(self.sprite)
-        player = pygame.transform.scale(player, self.player_size)
-        screen.blit(player, (self.position_x, self.position_y))
+        screen.blit(self.sprite, (self.position_x, self.position_y))
     
     # Task 2.4 - create move method
     def move(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
             self.position_y -= 5
+            # Task 2.5 - bonus task: make poco turn when moving
+            self.image = "images/player/poco_up.png"
             
         elif keys[pygame.K_a]:
             self.position_x -= 5
+            # Task 2.5 - bonus task: make poco turn when moving
+            self.image = "images/player/poco_left.png"
             
         elif keys[pygame.K_s]:
             self.position_y += 5
-            
+            # Task 2.5 - bonus task: make poco turn when moving
+            self.image = "images/player/poco_down.png"
+
         elif keys[pygame.K_d]:
             self.position_x += 5
-    
+            # Task 2.5 - bonus task: make poco turn when moving
+            self.image = "images/player/poco_right.png"
 
 # Task 2.2 - create a player instance and print out the properties
 player = Player()
-print(player.sprite, player.position_x, player.position_y, player.player_size)
+print(player.sprite, player.position_x, player.position_y, player.size)
 
 # Task 2.3 - draw the player
 player.draw()
+
+# Task 3.1 - create a food class
+class Food:
+    def __init__(self, name, image, position_x, position_y):
+        self.name = name
+        self.position_x = position_x
+        self.position_y = position_y
+        self.size = (50, 50)
+        self.image = image
+        self.sprite = pygame.transform.scale(pygame.image.load(image), self.size)
+    # Task 3.5 - create a draw method inside food
+    def draw(self):
+        screen.blit(self.sprite, (self.position_x, self.position_y))
+        
+# Task 3.2 - create a food dictionary
+foods = {
+    # "food" : ["sprite path", pos_x, pos_y]
+    # key : value
+    "banana": ["images/food/banana.png", 50, 500],
+    "grapes": ["images/food/grapes.png", 500, 400],
+    "peach": ["images/food/peach.png", 200, 400],
+    "strawberry": ["images/food/strawberry.png", 300, 20],
+    "sushi": ["images/food/sushi.png", 600, 200],
+    "watermelon": ["images/food/watermelon.png", 100, 70]
+}
+
+# Task 3.3 - initialise active foods
+active_foods = []
+for key, value in foods.items():
+    # key is the food name and the value[0] is sprite, value[1] is pos_x
+    # value[2] is pos_y
+    print(key, value[0], value[1], value[2])
+    active_foods.append(Food(key,value[0], value[1], value[2]))
 
 # Task 1.2 - create a running loop
 running = True
@@ -67,5 +106,9 @@ while running:
     # Task 2.4 - getting the player to move
     player.move()
     player.draw()
+    
+    # Task 3.6 - show all the foods in the active food list
+    for foods in active_foods:
+        foods.draw()
 
     pygame.display.update()
